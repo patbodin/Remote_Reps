0cf97cf1f0ee24b7a10aeba1e588fbd5fe694213
Pond <Patbodin30443@hotmail.com>
2018-02-04 12:43:14 +0700

    Added second comment

--- RemoteExample/src/ExampleRemote.java
+++ RemoteExample/src/ExampleRemote.java
@@ -4,6 +4,9 @@ public class ExampleRemote {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test My Remote Git");
+		
+		
+		//Test second thing
 	}
 
 }
